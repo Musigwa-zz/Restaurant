a3d30a17ce3046f96c90da35600df91262e9bf56
Musigwa <pacifique.musigwa@gmail.com>
2018-05-25 00:53:44 +0200

    Corrected the whole project

--- Input.ino
+++ Input.ino
@@ -1,111 +1,101 @@
-// const byte ROWS = 4, COLS = 4;
-// const byte colPins[COLS] = {A1, A2, A3, A4};
-// const byte rowPins[ROWS] = {5, 4, 3, A0};
-
-// const char hexaKeys[ROWS][COLS] = {
-//     {'1', '2', '3', 'A'},
-//     {'4', '5', '6', 'B'},
-//     {'7', '8', '9', 'C'},
-//     {'*', '0', '#', 'D'}};
-
-// Keypad myKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
-
-// void proccessOrder()
-// {
-//   String clientOrder = "";
-//   unsigned int Total = 0;
-//   bool Confirmed = false;
-//   byte i = 14, j = 0;
-//   for (;;)
-//   {
-//     char pressedKey = myKeypad.waitForKey();
-//     if (isDigit(pressedKey) && pressedKey != '0')
-//     {
-//       clientOrder += pressedKey;
-//       Total += tarrif[(pressedKey - 0) - 49][1].toInt(), lcd.setCursor(0, 0);
-//       if (i < 20)
-//       {
-//         lcd.print("YOU'VE CHOSEN:"), lcd.setCursor(i, j);
-//         if (i == 14 && j == 0)
-//         {
-//           lcd.print(pressedKey), i++;
-//         }
-//         else
-//         {
-//           lcd.print(","), lcd.print(pressedKey), i += 2;
-//         }
-//         clearRow(lcdSize, 3), lcd.print("Total:"), lcd.print(Total), lcd.print("RWF");
-//       }
-//       if (j < 1 && i == 19)
-//       {
-//         i = 0, j++;
-//       }
-//     }
-//     else if (pressedKey == '#')
-//     {
-//       if (Confirmed)
-//       {
-//         lcd.clear(), lcd.setCursor(2, 2), lcd.print("SENDING REQUEST...");
-//         if (isGsmReady())
-//         {
-//           String _buffer;
-//           SIM800L.println("AT+CMGF=1");
-//           SIM800L.println("AT+CMGS=\"" + number + "\"\r");
-//           _buffer = _readSerial(5000);
-//           while (clientOrder.length() > 0)
-//           {
-//             SIM800L.print(tarrif[clientOrder.substring(0, 1).toInt() - 1][0] + "(");
-//             SIM800L.print(tarrif[clientOrder.substring(0, 1).toInt() - 1][1] + "RWF)");
-//             clientOrder.remove(0, 1);
-//             if (clientOrder.length() == 0)
-//             {
-//               SIM800L.print(". Total payment: " + String(Total) + "RWF");
-//               break;
-//             }
-//             else
-//             {
-//               SIM800L.print(", ");
-//             }
-//           }
-//           _buffer += _readSerial(5000);
-//           SIM800L.println((char)26);
-//           _buffer += _readSerial(5000);
-//           if (((_buffer.indexOf("AT+CMGS")) != -1))
-//           {
-//             lcd.print("SENT!");
-//           }
-//           else
-//           {
-//             lcd.print("NOT SENT");
-//           }
-//         }
-//         else
-//         {
-//           lcd.print("NOT SENT");
-//         }
-//         delay(2000), lcd.clear();
-//         break;
-//       }
-//       else
-//       {
-//         if (Total != 0)
-//         {
-//           Confirmed = true, lcd.setCursor(0, 0), lcd.print("CONFIRM REQST:");
-//           lcd.setCursor(0, 2), lcd.print("YES [#]       NO [*]");
-//         }
-//         else
-//         {
-//           clearRow(lcdSize, 2), lcd.print("CAN'T SEND EMPTY REQ"), delay(2000);
-//           lcd.clear();
-//           break;
-//         }
-//       }
-//     }
-//     else if (pressedKey == '*' && Total != 0)
-//     {
-//       Confirmed = false, clearRow(lcdSize, 2), lcd.print("ORDER CANCELED!");
-//       delay(2000), lcd.clear(), Total = 0, clientOrder = "";
-//       break;
-//     }
-//   }
-// };
+void proccessOrder()
+{
+  String clientOrder = "";
+  unsigned int Total = 0;
+  bool Confirmed = false;
+  byte i = 14, j = 0;
+  for (;;)
+  {
+    char pressedKey = myKeypad.waitForKey();
+    if (isDigit(pressedKey) && pressedKey != '0')
+    {
+      clientOrder += pressedKey;
+      Total += atoi(tarrif[(pressedKey - 0) - 49][1]), lcd.setCursor(0, 0);
+      if (i < 20)
+      {
+        lcd.print("YOU'VE CHOSEN:"), lcd.setCursor(i, j);
+        if (i == 14 && j == 0)
+        {
+          lcd.print(pressedKey), i++;
+        }
+        else
+        {
+          lcd.print(","), lcd.print(pressedKey), i += 2;
+        }
+        clearRow(lcdSize, 3), lcd.print("Total:"), lcd.print(Total), lcd.print("RWF");
+      }
+      if (j < 1 && i == 19)
+      {
+        i = 0, j++;
+      }
+    }
+    else if (pressedKey == '#')
+    {
+      if (Confirmed)
+      {
+        lcd.clear(), lcd.setCursor(2, 2), lcd.print("SENDING REQUEST...");
+        if (isGsmReady())
+        {
+          String _buffer;
+          SIM800L.println("AT+CMGF=1");
+          SIM800L.println("AT+CMGS=\"" + number + "\"\r");
+          _buffer = _readSerial(5000);
+          while (clientOrder.length() > 0)
+          {
+            SIM800L.print(tarrif[clientOrder.substring(0, 1).toInt() - 1][0]);
+            SIM800L.print("(");
+            SIM800L.print(tarrif[clientOrder.substring(0, 1).toInt() - 1][1]);
+            SIM800L.print("RWF)");
+            clientOrder.remove(0, 1);
+            if (clientOrder.length() == 0)
+            {
+              SIM800L.print(". Total payment: " + String(Total) + "RWF");
+              break;
+            }
+            else
+            {
+              SIM800L.print(", ");
+            }
+          }
+          _buffer += _readSerial(5000);
+          SIM800L.println((char)26);
+          _buffer += _readSerial(5000);
+          if (((_buffer.indexOf("AT+CMGS")) != -1))
+          {
+            lcd.print("SENT!");
+          }
+          else
+          {
+            lcd.print("NOT SENT");
+          }
+        }
+        else
+        {
+          lcd.print("NOT SENT");
+        }
+        delay(2000), lcd.clear();
+        break;
+      }
+      else
+      {
+        if (Total != 0)
+        {
+          Confirmed = true, lcd.setCursor(0, 0), lcd.print("CONFIRM REQST:");
+          lcd.setCursor(0, 2), lcd.print("YES [#]       NO [*]");
+        }
+        else
+        {
+          clearRow(lcdSize, 2), lcd.print("CAN'T SEND EMPTY REQ"), delay(2000);
+          lcd.clear();
+          break;
+        }
+      }
+    }
+    else if (pressedKey == '*' && Total != 0)
+    {
+      Confirmed = false, clearRow(lcdSize, 2), lcd.print("ORDER CANCELED!");
+      delay(2000), lcd.clear(), Total = 0, clientOrder = "";
+      break;
+    }
+  }
+};
--- Restaurant.ino
+++ Restaurant.ino
@@ -1,54 +1,107 @@
-// #include <LiquidCrystal.h>
-// #include <Keypad.h>
-// #include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+#include <SoftwareSerial.h>
 
-// #define RX 10
-// #define TX 11
-// #define BAUD 9600
+#define nextTimeout 2000
+#define RX 10
+#define TX 11
+#define BAUD 9600
 
-// const String tarrif[9][9] = {
-//     {"Umugati", "200"},
-//     {"Icyayi", "200"},
-//     {"Umureti", "1500"},
-//     {"Special", "2000"},
-//     {"Ibishyimbo", "400"},
-//     {"Ifiriti", "1000"},
-//     {"Inyama", "1500"},
-//     {"Umuceri", "500"},
-//     {"Isosi", "200"}};
+unsigned long oldTime = 0;
+const String number = "+250788228892";
+byte qin = 0;
+bool c = false;
+const byte lcdSize[2] = {20, 4};
+const char *tarrif[8][8] = {
+    {"Umugati", "200"},
+    {"Icyayi", "200"},
+    {"Umureti", "1500"},
+    {"Special", "2000"},
+    {"Ibishyimbo", "400"},
+    {"Ifiriti", "1000"},
+    {"Inyama", "1500"},
+    {"Umuceri", "500"}};
 
-// const String number = "+250788228892";
-// const byte lcdSize[2] = {20, 4};
-// LiquidCrystal lcd(13, 12, 9, 8, 7, 6);
-// SoftwareSerial SIM800L(TX, RX);
+const byte ROWS = 4, COLS = 4;
+const byte colPins[COLS] = {A1, A2, A3, A4};
+const byte rowPins[ROWS] = {5, 4, 3, A0};
 
-// void setup()
-// {
-//   lcdsetup(), lcd.setCursor(0, 0), lcd.print("WELCOME IN OUR HOTEL"), SIM800L.begin(BAUD);
-//   lcd.setCursor(0, 1), lcd.print("CHECKING THE GSM..."), lcd.setCursor(3, 2);
-//   lcd.print(isGsmReady() ? "GSM IS READY!" : "NOT CONNECTED!"), delay(2000), lcd.clear();
-// };
+const char hexaKeys[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
-// void loop()
-// {
-//   proccessOrder();
-// };
+Keypad myKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+SoftwareSerial SIM800L(TX, RX);
+LiquidCrystal lcd(13, 12, 9, 8, 7, 6);
 
-// void lcdsetup()
-// {
-//   lcd.begin(lcdSize[0], lcdSize[1]), delay(50), lcd.clear();
-// };
+void setup()
+{
+  lcdsetup(), lcd.setCursor(0, 0), lcd.print("WELCOME IN OUR HOTEL"), SIM800L.begin(BAUD);
+  lcd.setCursor(0, 1), lcd.print("CHECKING THE GSM..."), lcd.setCursor(3, 2);
+  lcd.print(isGsmReady() ? "GSM IS READY!" : "NOT CONNECTED!"), delay(2000), lcd.clear();
+};
 
-// bool clearRow(const byte scrnSize[2], const byte R2Del)
-// {
-//   if (R2Del >= 0 && R2Del < scrnSize[1])
-//   {
-//     lcd.setCursor(0, R2Del);
-//     for (byte i = 0; i <= (scrnSize[0] - 1); i++)
-//     {
-//       lcd.print(" ");
-//     }
-//     lcd.setCursor(0, R2Del);
-//     return true;
-//   }
-// };
+void loop()
+{
+  char *key = myKeypad.getKey();
+  if (key)
+  {
+    if (key == 'A')
+    {
+      lcd.clear();
+      proccessOrder();
+    }
+  }
+  else
+  {
+    if (millis() - oldTime > nextTimeout)
+    {
+      lcd.clear();
+      if (c)
+      {
+        qin = 0;
+        c = !c;
+      }
+      else
+      {
+        qin = 4;
+        c = !c;
+      }
+      oldTime = millis();
+    }
+    showTarrif(qin);
+  }
+};
+
+void lcdsetup()
+{
+  lcd.begin(lcdSize[0], lcdSize[1]), delay(50), lcd.clear();
+};
+
+bool clearRow(const byte scrnSize[2], const byte R2Del)
+{
+  if (R2Del >= 0 && R2Del < scrnSize[1])
+  {
+    lcd.setCursor(0, R2Del);
+    for (byte i = 0; i <= (scrnSize[0] - 1); i++)
+    {
+      lcd.print(" ");
+    }
+    lcd.setCursor(0, R2Del);
+    return true;
+  }
+};
+
+void showTarrif(byte ind)
+{
+  lcd.setCursor(0, 0), lcd.print(ind + 1), lcd.print("."), lcd.print(tarrif[ind][0]);
+  lcd.print("="), lcd.print(tarrif[ind + 0][1]), lcd.print("RWF");
+  lcd.setCursor(0, 1), lcd.print(ind + 2), lcd.print("."), lcd.print(tarrif[ind + 1][0]);
+  lcd.print("="), lcd.print(tarrif[ind + 1][1]), lcd.print("RWF");
+  lcd.setCursor(0, 2), lcd.print(ind + 3), lcd.print("."), lcd.print(tarrif[ind + 2][0]);
+  lcd.print("="), lcd.print(tarrif[ind + 2][1]), lcd.print("RWF");
+  lcd.setCursor(0, 3), lcd.print(ind + 4), lcd.print("."), lcd.print(tarrif[ind + 3][0]);
+  lcd.print("="), lcd.print(tarrif[ind + 3][1]), lcd.print("RWF");
+};
